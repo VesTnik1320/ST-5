25232d92f2c0e4bfdf622589dd97e035a1feb89b
VesTnik1320 <zhurin.ivan33@outlook.com>
2026-06-02 20:01:41 +0300

    Update tests

--- src/test/java/com/mycompany/app/SqrtTest.java
+++ src/test/java/com/mycompany/app/SqrtTest.java
@@ -8,75 +8,81 @@ import org.junit.Test;
 
 public class SqrtTest {
 
-    private static final double EPS = 1e-6;
+    private static final double EPS = 1e-9;
 
     @Test
-    public void average_returnsArithmeticMean() {
-        Sqrt s = new Sqrt(1.0);
-        assertEquals(3.0, s.average(2.0, 4.0), EPS);
+    public void CanCalculateAverageOfTwoNumbers() {
+        Sqrt sqrt = new Sqrt(0.0);
+        assertEquals(5.0, sqrt.average(2.0, 8.0), EPS);
     }
 
     @Test
-    public void average_handlesNegativeNumbers() {
-        Sqrt s = new Sqrt(1.0);
-        assertEquals(-0.5, s.average(-2.0, 1.0), EPS);
+    public void CanHandleNegativeNumbersInAverage() {
+        Sqrt sqrt = new Sqrt(0.0);
+        assertEquals(-1.0, sqrt.average(-3.0, 1.0), EPS);
     }
 
     @Test
-    public void good_isTrueWhenGuessSquaredCloseToX() {
-        Sqrt s = new Sqrt(1.0);
-        assertTrue(s.good(2.0, 4.0));
+    public void CheckGoodReturnsTrueWhenGuessIsCloseEnough() {
+        Sqrt sqrt = new Sqrt(0.0);
+        assertTrue(sqrt.good(2.000000001, 4.0));
     }
 
     @Test
-    public void good_isFalseWhenGuessIsFarFromRoot() {
-        Sqrt s = new Sqrt(1.0);
-        assertFalse(s.good(1.0, 100.0));
+    public void CheckGoodReturnsFalseWhenGuessIsFar() {
+        Sqrt sqrt = new Sqrt(0.0);
+        assertFalse(sqrt.good(2.5, 4.0));
     }
 
     @Test
-    public void improve_movesGuessTowardSquareRootOfX() {
-        Sqrt s = new Sqrt(1.0);
-        double next = s.improve(1.0, 9.0);
-        assertEquals(5.0, next, EPS);
+    public void CanImproveGuessTowardsSquareRoot() {
+        Sqrt sqrt = new Sqrt(0.0);
+        double improved = sqrt.improve(1.0, 9.0);
+        assertEquals(5.0, improved, EPS);
     }
 
     @Test
-    public void iter_returnsGuessImmediatelyWhenGood() {
-        Sqrt s = new Sqrt(1.0);
-        assertEquals(3.0, s.iter(3.0, 9.0), EPS);
+    public void CheckIterReturnsGuessImmediatelyIfGood() {
+        Sqrt sqrt = new Sqrt(0.0);
+        double result = sqrt.iter(3.0, 9.0);
+        assertEquals(3.0, result, EPS);
     }
 
     @Test
-    public void iter_convergesThroughRecursiveCalls() {
-        Sqrt s = new Sqrt(1.0);
-        double r = s.iter(1.0, 2.0);
-        assertEquals(Math.sqrt(2.0), r, 1e-7);
+    public void CanIterateUntilConvergence() {
+        Sqrt sqrt = new Sqrt(0.0);
+        double result = sqrt.iter(1.0, 2.0);
+        assertEquals(Math.sqrt(2.0), result, 1e-8);
     }
 
     @Test
-    public void calc_computesSquareRootOfTwo() {
-        Sqrt s = new Sqrt(2.0);
-        assertEquals(Math.sqrt(2.0), s.calc(), 1e-7);
+    public void CanComputeSquareRootOfTwo() {
+        Sqrt sqrt = new Sqrt(2.0);
+        double result = sqrt.calc();
+        assertEquals(Math.sqrt(2.0), result, 1e-8);
     }
 
     @Test
-    public void calc_computesSquareRootOfPerfectSquare() {
-        Sqrt s = new Sqrt(25.0);
-        assertEquals(5.0, s.calc(), 1e-7);
+    public void CanComputeSquareRootOfPerfectSquare() {
+        Sqrt sqrt = new Sqrt(25.0);
+        assertEquals(5.0, sqrt.calc(), EPS);
     }
 
     @Test
-    public void calc_usesConstructorArg() {
-        Sqrt s = new Sqrt(16.0);
-        assertEquals(4.0, s.calc(), 1e-7);
+    public void CheckCalcHandlesVerySmallNumber() {
+        Sqrt sqrt = new Sqrt(1e-12);
+        assertEquals(1e-6, sqrt.calc(), 1e-12);
     }
 
     @Test
-    public void good_boundaryNearDelta() {
-        Sqrt s = new Sqrt(1.0);
-        double x = 4.0;
-        double guess = 2.0 - 1e-5;
-        assertFalse(s.good(guess, x));
+    public void CheckCalcHandlesVeryLargeNumber() {
+        Sqrt sqrt = new Sqrt(1e12);
+        assertEquals(1e6, sqrt.calc(), 1e-4);
+    }
+
+    @Test
+    public void CanComputeSquareRootOfZero() {
+        Sqrt sqrt = new Sqrt(0.0);
+        assertEquals(0.0, sqrt.calc(), EPS);
     }
 }
